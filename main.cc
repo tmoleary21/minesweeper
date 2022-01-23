59826c53a5e04dc554bed47e5eb4e2570e6f6e9c
Tyson O'Leary <tmoleary@colostate.edu>
2022-01-22 18:42:32 -0700

    Create default and empty square textures

--- main.cc
+++ main.cc
@@ -4,7 +4,26 @@
 using namespace std;
 
 int main(){
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Minesweeper by tmoleary21");
+
+    sf::Texture defaultTexture;
+    if(!defaultTexture.loadFromFile("textures/default.png")){
+        cerr << "Error: Could not find file\n";
+        return 0;
+    }
+    sf::Texture emptyTexture;
+    if(!emptyTexture.loadFromFile("textures/empty.png")){
+        cerr << "Error: Could not find file\n";
+        return 0;
+    }
+    
+    sf::Sprite defaultSprite;
+    defaultSprite.setTexture(defaultTexture);
+    defaultSprite.setPosition(sf::Vector2f(20,20));
+    sf::Sprite emptySprite;
+    emptySprite.setTexture(emptyTexture);
+    emptySprite.setPosition(sf::Vector2f(60,20));
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,7 +35,8 @@ int main(){
 
         window.clear();
         //Draw here
-        
+        window.draw(defaultSprite);
+        window.draw(emptySprite);
 
         window.display();
     }
